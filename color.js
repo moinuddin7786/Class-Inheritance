c7d48c775412eb32996f5f4e6b5ef9876ec20e8e
MD Khaja Moinuddin <88581014+moinuddin7786@users.noreply.github.com>
2021-11-17 22:31:04 +0530

    Update color.js

--- color.js
+++ color.js
@@ -1,11 +1,11 @@
 //import using reqird
-const Shape = require("./shape")
+const Shape-require("./shape")
 
 
 //declare class
 class Circle extends Shape{
     super(radius){
-        this.radius = radius;
+        this.radius = "radius";
     }
     calculateArea(){
         console.log('Area');
